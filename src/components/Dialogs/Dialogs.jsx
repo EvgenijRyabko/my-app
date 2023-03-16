c2321932f609fa0ba5d0620b4992d1cd9d4e70dd
Evgenij Ryabko <jeniaryanko@mail.ru>
2023-03-16 12:57:37 +0300

    DialogsRefactoring

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import classes from './Dialogs.module.css';
+
+const DialogItem = ({ name }) => {
+    return (
+        <section className={ classes.dialog }>
+            { name }
+        </section>
+    );
+}
 
 const Dialogs = () => {
     return (
-        <>
-        </>
+        <section className={ classes.dialogs }>
+            <div className={ classes['dialogs-items']}>
+                <div className={ classes.dialog }>Andrew</div>
+                <div className={ classes.dialog }>Mark</div>
+                <div className={ classes.dialog }>Slavyan</div>
+                <div className={ classes.dialog }>Sveta</div>
+                <div className={ classes.dialog }>Yarik</div>
+            </div>
+            <div className={ classes.messages}>
+                <div className={ classes.message }>Hi</div>
+                <div className={ classes.message }>How are your course?</div>
+                <div className={ classes.message }>Yo</div>
+            </div>
+        </section>
     );
 }
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,26 +1,21 @@
 import React from "react";
 import Classes from "./Navbar.module.css";
 import { NavLink } from 'react-router-dom';
+import data from '../../data/nav-data.json';
 
 const Navbar = () => {
   return (
     <nav className={Classes.nav}>
       <ul>
-        <li>
-          <NavLink to="/profile">Profile</NavLink>
-        </li>
-        <li>
-          <NavLink to="/dialogs">Dialogs</NavLink>
-        </li>
-        <li>
-          <NavLink to="/news">News</NavLink>
-        </li>
-        <li>
-          <NavLink to="/music">Music</NavLink>
-        </li>
-        <li>
-          <NavLink to="/settings">Settings</NavLink>
-        </li>
+        {
+          data.map((el, i) => {
+            return (
+              <li key={ i }>
+                <NavLink to={ el.to }>{ el.text }</NavLink>
+              </li>
+            )
+          })
+        }
       </ul>
     </nav>
   );

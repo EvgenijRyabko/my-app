a43615e8b7cf3580bc91c5031fe214fe06d247ab
Evgenij Ryabko <jeniaryanko@mail.ru>
2023-03-24 10:19:56 +0300

    State_OOP_Refactored

--- src/data/state.js
+++ src/data/state.js
@@ -2,35 +2,34 @@ import dialogsPage from "./dialogsPage/dialogsPage";
 import profilePage from "./profilePage/profilePage";
 import sidebar from "./sidebar/sidebar";
 
-let rerenderEntireTree = () => {}
-
-let state = {
-    profilePage,
-    dialogsPage,
-    sidebar
+let store = {
+    _state: {
+        profilePage,
+        dialogsPage,
+        sidebar
+    },
+    getState(){
+        return this._state;
+    },
+    _callSubscriber() {},
+    addPost(){
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            likesCount: 0
+        };
+    
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText){
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer){
+        this._callSubscriber = observer;
+    }
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        likesCount: 0
-    };
-
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    rerenderEntireTree(state);
-}
-
-export const updateNewPostText  = (newText) => {
-    state.profilePage.newPostText = newText;
-    rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) => {
-    rerenderEntireTree = observer;
-}
-
-
-
-export default state;
+export default store;
--- src/index.js
+++ src/index.js
@@ -3,22 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
-import { addPost, updateNewPostText, subscribe } from './data/state';
-import state from './data/state';
+import store from './data/state';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 export let rerenderEntireTree = (state) => {
     root.render(
         <React.StrictMode>
-          <App state={state} addPost={addPost} updateNewPostText={updateNewPostText}/>
+          <App state={state} addPost={store.addPost.bind(store)} updateNewPostText={store.updateNewPostText.bind(store)}/>
         </React.StrictMode>
       );
 }
 
-rerenderEntireTree(state);
+rerenderEntireTree(store.getState());
 
-subscribe(rerenderEntireTree);
+store.subscribe(rerenderEntireTree);
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals

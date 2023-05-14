429f2c7de21303a36befb825a3232439232f3cb1
EvgenijRyabko <jeniaryabko@bk.ru>
2023-05-14 23:00:49 +0300

    App_Refactored

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,34 +1,35 @@
-import "./App.css";
-import Header from "./Header/Header";
-import NavbarContainer from "./Navbar/NavbarContainer";
-import Profile from "./Profile/Profile";
-import DialogsContainer from "./Dialogs/DialogsContainer";
-import News from "./News/News";
-import Music from "./Music/Music";
-import Settings from "./Settings/Settings";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import UsersContainer from "./Users/UsersContainer";
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './Header/Header';
+import NavbarContainer from './Navbar/NavbarContainer';
+import Profile from './Profile/Profile';
+import DialogsContainer from './Dialogs/DialogsContainer';
+import News from './News/News';
+import Music from './Music/Music';
+import Settings from './Settings/Settings';
+import UsersContainer from './Users/UsersContainer';
 
-const App = () => {
-	return (
-		<BrowserRouter>
-			<div className="app-wrapper">
-				<Header />
-				<NavbarContainer />
-				<div className="app-wrapper-content">
-					<Routes>
-						<Route path='/' element={<Profile />} />
-						<Route path='/dialogs' element={<DialogsContainer />} />
-						<Route path='/profile' element={<Profile />} />
-						<Route path='/users' element={<UsersContainer />} />
-						<Route path='/news' element={<News />} />
-						<Route path='/music' element={<Music />} />
-						<Route path='/settings' element={<Settings />} />
-					</Routes>
-				</div>
-			</div>
-		</BrowserRouter>
-	);
-};
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <NavbarContainer />
+        <div className="app-wrapper-content">
+          <Routes>
+            <Route path="/" element={<Profile />} />
+            <Route path="/dialogs" element={<DialogsContainer />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/users" element={<UsersContainer />} />
+            <Route path="/news" element={<News />} />
+            <Route path="/music" element={<Music />} />
+            <Route path="/settings" element={<Settings />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+}
 
 export default App;
